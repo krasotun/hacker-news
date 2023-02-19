48673009b752206ac6f9fe64494cbede49b64ac5
krasotun <servrostov@gmail.com>
2023-02-19 15:14:40 +0300

    fix: loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import {
+  Event,
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+} from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'hacker-news';
+  loading = false;
+  constructor(private _router: Router) {
+    this._router.events.subscribe((res: Event) => {
+      this._setLoaderStatus(res);
+    });
+  }
+  private _setLoaderStatus(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+    if (
+      routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError
+    ) {
+      this.loading = false;
+    }
+  }
 }
