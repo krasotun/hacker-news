1f0d2adcd0cae2fb66918171d42cd43478c52d5d
krasotun <servrostov@gmail.com>
2023-02-19 17:05:20 +0300

    ref: rx js for loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import {
   Event,
   NavigationCancel,
@@ -7,28 +7,41 @@ import {
   NavigationStart,
   Router,
 } from '@angular/router';
+import { BehaviorSubject, Subject, Subscription, takeUntil } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  loading = false;
+export class AppComponent implements OnDestroy {
+  private _routerSubscription!: Subscription | null;
+  private readonly _destroySubject = new Subject<void>();
+  private readonly _loadingSubject = new BehaviorSubject<boolean>(false);
+  loading$ = this._loadingSubject.pipe(takeUntil(this._destroySubject));
+
   constructor(private _router: Router) {
-    this._router.events.subscribe((res: Event) => {
+    this._routerSubscription = this._router.events.subscribe((res: Event) => {
       this._setLoaderStatus(res);
     });
   }
   private _setLoaderStatus(routerEvent: Event): void {
     if (routerEvent instanceof NavigationStart) {
-      this.loading = true;
+      this._loadingSubject.next(true);
     }
     if (
       routerEvent instanceof NavigationEnd ||
       routerEvent instanceof NavigationCancel ||
       routerEvent instanceof NavigationError
     ) {
-      this.loading = false;
+      this._loadingSubject.next(false);
+    }
+  }
+  ngOnDestroy(): void {
+    this._destroySubject.next();
+    this._destroySubject.complete();
+    if (this._routerSubscription) {
+      this._routerSubscription.unsubscribe();
+      this._routerSubscription = null;
     }
   }
 }
